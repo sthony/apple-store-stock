c9ddd81c222394ea4a686ae3ecad7a4fa37f6fb7
Steven <steven.anthony@finaccel.co>
2022-02-08 13:09:01 +0700

    (feat) add validation wrong route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,14 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/healthcheck', function(req, res, next) {
-  return res.json({
+  return res.status(200).json({
     status: 'ok',
+  });
+});
+
+router.get('*', (req, res) => {
+  return res.status(404).json({
+    message: 'wrong route',
   });
 });
 
